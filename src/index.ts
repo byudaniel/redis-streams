bd488ef18e171b9f1331de66152fdefdb64874d7
Daniel O'Neill <daniel.oneill@motivaventures.com>
2022-06-07 10:58:40 -0400

    Fix ack, upgrade ioredis

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { nanoid } from 'nanoid'
 interface SubscribeOpts {
   pollInterval?: number
   subscribeFromStart: boolean
+  consumerName?: string
+  disableCreateGroup?: boolean
 }
 
 export class RedisStreams {
@@ -29,10 +31,10 @@ export class RedisStreams {
 
   private async ackMessage(
     streamName: string,
-    consumerGroupName: string,
+    consumerName: string,
     messageId: string
   ) {
-    await this.redis.xack(streamName, consumerGroupName, messageId)
+    await this.redis.xack(streamName, consumerName, messageId)
   }
 
   subscribe(
@@ -48,28 +50,30 @@ export class RedisStreams {
     opts?: SubscribeOpts
   ) {
     setTimeout(async () => {
-      try {
-        await this.redis.xgroup(
-          'CREATE',
-          streamName,
-          groupName,
-          opts?.subscribeFromStart ? 0 : '$',
-          'MKSTREAM'
-        )
-      } catch (err) {
-        if (
-          err instanceof Error &&
-          !err.message.toLowerCase().includes('already exists')
-        ) {
-          throw err
+      if (!opts?.disableCreateGroup) {
+        try {
+          await this.redis.xgroup(
+            'CREATE',
+            streamName,
+            groupName,
+            opts?.subscribeFromStart ? 0 : '$',
+            'MKSTREAM'
+          )
+        } catch (err) {
+          if (
+            err instanceof Error &&
+            !err.message.toLowerCase().includes('already exists')
+          ) {
+            throw err
+          }
         }
       }
 
-      const consumerGroupName = nanoid()
+      const consumerName = opts?.consumerName || nanoid()
       const data = await this.redis.xreadgroup(
         'GROUP',
         groupName,
-        consumerGroupName,
+        consumerName,
         'BLOCK',
         opts?.pollInterval || 60000,
         'COUNT',
@@ -85,14 +89,19 @@ export class RedisStreams {
             handler({
               message: JSON.parse(inner[1][1]),
               ack: () => {
-                return this.ackMessage(streamName, consumerGroupName, inner[0])
+                return this.ackMessage(streamName, consumerName, inner[0])
               },
             })
           }
         }
       }
 
-      this.subscribe(streamName, groupName, handler, opts)
+      this.subscribe(streamName, groupName, handler, {
+        subscribeFromStart: false,
+        ...opts,
+        consumerName,
+        disableCreateGroup: true,
+      })
     }, 0)
   }
 }
